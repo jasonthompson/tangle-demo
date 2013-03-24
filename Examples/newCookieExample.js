f9eb3c433c3f088182b26abbf9b69f8e4119ca34
Jason Thompson <jason@jthompson.ca>
2013-03-24 09:30:14 -0400

    Added dailyIntake and percentageOfDailyIntake for a male aged 31-50.
    Next step calculate daily intake for different activity levels.

--- Examples/newCookieExample.js
+++ Examples/newCookieExample.js
@@ -9,9 +9,11 @@ function setUpTangle () {
         initialize: function () {
             this.cookies = 4;
             this.caloriesPerCookie = 50;
+            this.dailyIntake = 2900;
         },
         update: function () {
             this.calories = this.cookies * this.caloriesPerCookie;
+            this.percentageOfDailyIntake = Math.round(this.calories / this.dailyIntake * 100);
         }
     });
 }
