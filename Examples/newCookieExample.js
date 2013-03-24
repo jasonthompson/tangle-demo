77b3a7f1a155f4804d1dfd4e03cd6a02f9771cb1
Jason Thompson <jason@jthompson.ca>
2013-03-24 11:29:41 -0400

    Created initial data model so changing between male and female changes daily recommended intake.
    To do: choose between activity levels.

--- Examples/newCookieExample.js
+++ Examples/newCookieExample.js
@@ -1,3 +1,14 @@
+var dailyRecommendedIntakeTable = {
+    table: [
+        {ageRange: [31,50], male: [2350, 2600, 2900], female: [1800, 2000, 2250]}
+ ],
+
+    lookup: function(age, gender, activityLevel){
+        var gender = ["male", "female"][gender];
+        return this.table[age][gender][activityLevel];
+        }
+};
+
 function setUpTangle () {
 
     var element = document.getElementById("example");
@@ -9,10 +20,12 @@ function setUpTangle () {
         initialize: function () {
             this.cookies = 4;
             this.caloriesPerCookie = 50;
-            this.dailyIntake = 2900;
             this.gender = 0;
+            this.activityLevel = 2;
+            this.dailyIntake = dailyRecommendedIntakeTable.lookup(0,this.gender,this.activityLevel);
         },
         update: function () {
+            this.dailyIntake = dailyRecommendedIntakeTable.lookup(0,this.gender,this.activityLevel);
             this.calories = this.cookies * this.caloriesPerCookie;
             this.percentageOfDailyIntake = Math.round(this.calories / this.dailyIntake * 100);
         }
