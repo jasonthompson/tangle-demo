b55a12f29c37b6930ff508f4a2bc9d67cb5e5292
Jason Thompson <jason@jthompson.ca>
2013-03-25 05:39:28 -0400

    Removed the age array from data structure, made last age range "71 and up" in selector and made a few cosmetic changes.

--- public/js/newCookieExample.js
+++ public/js/newCookieExample.js
@@ -9,22 +9,18 @@
  */
 var dailyRecommendedIntakeTable = {
     table: [
-{ageRange: [2, 3], male: [1100, 1350, 1500], female: [1100, 1250, 1400]},
-{ageRange: [4, 5], male: [1250, 1450, 1650], female: [1200, 1350, 1500]},
-{ageRange: [6, 7], male: [1400, 1600, 1800], female: [1300, 1500, 1700]},
-{ageRange: [8, 9], male: [1500, 1750, 2000], female: [1400, 1600, 1850]},
-{ageRange: [10, 11], male: [1700, 2000, 2300], female: [1500, 1800, 2050]},
-{ageRange: [12, 13], male: [1900, 2250, 2600], female: [1700, 2000, 2250]},
-{ageRange: [14, 16], male: [2300, 2700, 3100], female: [1750, 2100, 2350]},
-{ageRange: [17, 18], male: [2450, 2900, 3300], female: [1750, 2100, 2400]},
-{ageRange: [19, 30], male: [2500, 2700, 3000], female: [1900, 2100, 2350]},
-{ageRange: [31, 50], male: [2350, 2600, 2900], female: [1900, 2100, 2350]},
-{ageRange: [51, 70], male: [2150, 2350, 2650], female: [1650, 1850, 2100]},
-/* Health Canada's chart ends at 71 and over. I added 150 as the upper bound to
- * make the lookup algorithm simpler.
- */
-
-{ageRange: [71, 150], male: [2000, 2200, 2500], female: [1550, 1750, 2000]}
+{male: [1100, 1350, 1500], female: [1100, 1250, 1400]}, // 2-3 yrs
+{male: [1250, 1450, 1650], female: [1200, 1350, 1500]}, // 4-5 yrs
+{male: [1400, 1600, 1800], female: [1300, 1500, 1700]}, // 6-7 yrs
+{male: [1500, 1750, 2000], female: [1400, 1600, 1850]}, // 8-9 yrs
+{male: [1700, 2000, 2300], female: [1500, 1800, 2050]}, // 10-11 yrs
+{male: [1900, 2250, 2600], female: [1700, 2000, 2250]}, // 12-13 yrs
+{male: [2300, 2700, 3100], female: [1750, 2100, 2350]}, // 14-16 yrs
+{male: [2450, 2900, 3300], female: [1750, 2100, 2400]}, // 17-18 yrs
+{male: [2500, 2700, 3000], female: [1900, 2100, 2350]}, // 19-30 yrs
+{male: [2350, 2600, 2900], female: [1900, 2100, 2350]}, // 31-50 yrs
+{male: [2150, 2350, 2650], female: [1650, 1850, 2100]}, // 51-70 yrs
+{male: [2000, 2200, 2500], female: [1550, 1750, 2000]} // 71 and up.
 ],
 
     lookup: function(age, gender, activityLevel){
